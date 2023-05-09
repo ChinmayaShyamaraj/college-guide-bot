1a2f69badbee9fec0bce126f720102e6a5ed0eab
Chinmaya <Chinmaya1024@gmail.com>
2023-05-09 09:44:36 +0530

    final commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+import tkinter as tk
+import time
 # Load the intents JSON file
 with open('intents.json') as file:
     data = json.load(file)
@@ -45,13 +46,45 @@ def chatbot_response(user_input):
             break
     return response
 
+import tkinter as tk
 
-query=""
-print("Hello! I am your campus assistance bot. Ask me anything!\n")
-while True:
-    query=input(">")
-    if query=='bye':
-        print(chatbot_response(query))
-        break
-    response=chatbot_response(query)
-    print(response)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack(fill='both', expand=True)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.text_output = tk.Text(self, wrap='word', height=10, state='disabled')
+        self.text_output.pack(fill='both', expand=True)
+
+        self.prompt_label = tk.Label(self, text="Enter input:")
+        self.prompt_label.pack()
+        self.prompt_entry = tk.Entry(self)
+        self.prompt_entry.pack(fill='x')
+
+        self.prompt_entry.bind('<Return>', self.submit_prompt)
+        self.prompt_entry.focus()
+
+    def submit_prompt(self, event):
+        input_text = self.prompt_entry.get()
+        self.prompt_entry.delete(0, 'end')
+        output_text = self.process_input(input_text)
+        self.text_output.configure(state='normal')
+        self.text_output.insert('end', f'\n\nYou: {input_text}\n{output_text}')
+        self.text_output.configure(state='disabled')
+        self.text_output.see('end')
+
+            
+        
+
+    def process_input(self, input_text):
+        # Do some processing on the input text here
+        output_text = "guideBot> " + chatbot_response(input_text)
+        return output_text
+
+root = tk.Tk()
+root.title("Campus guide bot")
+app = Application(master=root)
+app.mainloop()
